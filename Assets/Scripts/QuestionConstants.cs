2df361b2d79eabb04fed75744778e5474aad4178
PelleSwelle <72729955+PelleSwelle@users.noreply.github.com>
2022-11-25 17:42:52 +0100

    moved questions to separate file, implemented all conversations

--- Assets/Scripts/QuestionConstants.cs
+++ Assets/Scripts/QuestionConstants.cs
@@ -8,7 +8,7 @@ public class QuestionConstants
     {
         new Question(1, 0, 0,
             "Who are you to Olivia", new Answer(
-                "I'm her best friend. We do everything together, who could do this?"), true),
+                "I'm her best friend. We do everything together, who could do this?"), hasBranches: true),
         new Question(2, 1, 0,
             "What were you doing that day?", new Answer(
                 "Me and Olivia were just playing cards and talking about my relationship issues while playing cards")),
@@ -23,13 +23,13 @@ public class QuestionConstants
                 "We had an argument, that made me upset.")),
         new Question(5, 0, 0,
             "What was the argument about?", new Answer(
-                "I heard her speaking to another guys on the phone. Which made me angry because, she has a boyfriend"), true),
+                "I heard her speaking to another guys on the phone. Which made me angry because, she has a boyfriend"), hasBranches: true),
         new Question(6, 1, 0,
             "Who was on the phone?", new Answer(
-                "Her secret lover, I belive he's an Real-Estate Agent"), true),
+                "Her secret lover, I belive he's an Real-Estate Agent"), hasBranches: true),
         new Question(6, 2, 0,
             "When did you return to the house?", new Answer(
-                "I returned to the house an hour later, and saw someone running out of the house."), true),
+                "I returned to the house an hour later, and saw someone running out of the house."), hasBranches: true),
         new Question(7, 1, 1,
             "Do you know him?", new Answer(
                 "No, I just hear Olivia speak to him over the phone very often")),
@@ -44,7 +44,7 @@ public class QuestionConstants
                 "No he was not, he was going for the pizza, when I left the house.")),
         new Question(8, 11,  0,
             "Do you have any idea of who could have done this?", new Answer(
-                "It must have been that person running away! If only I knew who it was! How could he just do this"), false, true)
+                "It must have been that person running away! If only I knew who it was! How could he just do this"), isEndPoint: true)
     };
 
     public List<Question> officerQuestions = new List<Question>()
@@ -63,40 +63,41 @@ public class QuestionConstants
                     "Yes, the victim was stabbed multiple times by a kitchen knife")),
             new Question(5, 0, 0,
                 "Do you have any idea who the murderer is?", new Answer(
-                    "No, based on the information we have gathered from each of the suspects, the murderer could be any of them..."))
+                    "No, based on the information we have gathered from each of the suspects, the murderer could be any of them..."), isEndPoint: true)
     };
+
     public List<Question> jamesQuestions = new List<Question>()
     {
         new Question(1, 0, 0,
                 "Who are you to Olivia?", new Answer(
-                    "I'm her boyfriend for the past 3 years")),
+                    "I'm her boyfriend for the past 3 years"), hasBranches: true),
             new Question(2, 1, 0,
                 "How many were you guys that day?", new Answer(
                     "We were just three people")),
             new Question(2, 2, 0,
                 "What were you guys doing at the house?", new Answer(
                     "We were having fun talking about vacation ideas together")),
-            new Question(3, 0, 0,
+            new Question(3, 11, 0,
                 "Where were you at the time of the murder of Olivia?", new Answer(
-                    "I was on my way to gcollect the pizzas that we ordered beforehand.", false)),
+                    "I was on my way to gcollect the pizzas that we ordered beforehand.", _isTrue: false), hasBranches: true),
             new Question(4, 1, 0,
                 "Where was the pizzas ordered from?", new Answer(
                     "They were from Papa Jones")),
             new Question(4, 2, 0,
                 "Who ordered the pizzas?", new Answer(
-                    "It was Mary who ordered them! ")),
-            new Question(5, 0, 0,
+                    "It was Mary who ordered them! "), hasBranches: true),
+            new Question(5, 11, 0,
                 "How long did it take you to collect the pizzas?", new Answer(
-                    "It took me around an 45 minutes to get there", false)),
+                    "It took me around an 45 minutes to get there", _isTrue: false), hasBranches: true),
             new Question(6, 1, 0,
                 "What took you so long to get them?", new Answer(
-                    "I drove into too much traffic which caused a big delay", false)),
+                    "I drove into a lot of traffic which caused a big delay", _isTrue: false), hasBranches: true),
             new Question(6, 2, 0,
                 "Any reason why it was you that went to get them?", new Answer(
-                    "Because Mary ordered them, It must have been Mary who set this up!", false)),
+                    "Because Mary ordered them, It must have been Mary who set this up!", _isTrue: false), hasBranches: true),
             new Question(7, 1, 1,
                 "When did you return home with the pizzas?", new Answer(
-                    "I came home 1 hour later and was too late, all the cops were there", false)),
+                    "*sob* I came home an hour later and was too late, all the cops were there", false)),
             new Question(7, 1, 2,
                 "Who is Mary to you?", new Answer(
                     "I thought she was Olivias best friend. It  appears they argued a lot lately which seems strange")),
@@ -108,20 +109,21 @@ public class QuestionConstants
                     "I thought she was Olivias best friend. It  appears they argued a lot lately which seems strange")),
             new Question(8, 11, 0,
                 "Who do you suspect the murderer to be?", new Answer(
-                    "It has to be Mary! She setup all of this and called the cops"))
+                    "It has to be Mary! She setup all of this and called the cops", _isTrue: false), isEndPoint: true)
     };
+
     public List<Question> harryQuestions = new List<Question>()
     {
         new Question(1, 0, 0,
                 "Who are you to Olivia?", new Answer(
-                    "I am her real estate agent")),
+                    "I am her real estate agent"), hasBranches: true),
             new Question(2, 1, 0,
                 "How long have you known Olivia for?", new Answer(
-                    "About 6 months now")),
+                    "We have been in contact for about 6 months now.")),
             new Question(2, 2, 0,
                 "When did you last speak with Olivia?", new Answer(
                     "I called her last week", false)),
-            new Question(3, 0, 0,
+            new Question(3, 11, 0,
                 "Where were you at the time of the murder of Olivia?", new Answer(
                     "I was at home by myself.", false)),
             new Question(4, 1, 0,
@@ -130,30 +132,30 @@ public class QuestionConstants
             new Question(4, 2, 0,
                 "Did you and Olivia ever meet up?", new Answer(
                     "Occasionally yes")),
-            new Question(5, 0, 0,
+            new Question(5, 11, 0,
                 "You called Olivia on the phone on the night she died. What was the call about?", new Answer(
-                    "I- I was just talking with Olivia about selling me her house.")),
+                    "I- I was just talking with Olivia about selling me her house."), hasBranches: true),
             new Question(6, 1, 0,
                 "Why did you want to buy her house?", new Answer(
-                    "I buy houses I deem profitable as a long term investment. That's my job.")),
+                    "I buy houses I deem profitable as a long term investment. That's my job."), hasBranches: true),
             new Question(6, 2, 0,
                 "How did the call between the two of you end?", new Answer(
-                    "I heard an argument between Olivia and her friend which ended up in screams and smacks, so I hung up")),
+                    "I heard an argument between Olivia and her friend which ended up in screams and smacks, so I hung up"), hasBranches: true),
             new Question(7, 1, 1,
                 "Did olivia refuse to sell her house?", new Answer(
-                    "Yes, she kept mentioning that she will keep it as a memory of her")),
+                    "Yes, she kept mentioning that she will keep it as a memory of her grandmother who passed away.")),
             new Question(7, 1, 2,
                 "Who will own the house now?", new Answer(
-                    "Uh due to her unfortunate death, the house will go up for sale")),
+                    "Well, because she told me that her grandmother passed away the house will eventually be sold to the highest bidder.")),
             new Question(7, 2, 1,
                 "Did you go to the house afterwards?", new Answer(
-                    "No! I did not want to get involved in their drama, so I remained silent")),
+                    "No! I did not want to get involved in their drama. I dont know what happened that night.")),
             new Question(7, 2, 2,
-                "Did you have a secret relationship with Olivia?", new Answer(
+                "Were you romantically involved with Olivia?", new Answer(
                     "Uhm I guess you could say that. We started to like each other more over time")),
             new Question(8, 11, 0,
-                "Who do you suspect the murderer to be?", new Answer(
-                    "I have no idea! I did not expect this to ever even happen. I should'nt even be here!"))
+                "Do you have any idea, who could have done this?", new Answer(
+                    "I have no idea! I did not expect this to ever even happen. I should'nt even be here!"), isEndPoint: true)
     };
 
 
